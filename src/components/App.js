dca7ed1087bd5131af34bd8de60ceb70d33bea26
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-21 15:37:31 +0530

    Organised directories

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import ListBooks from './components/ListBooks';
-import SearchBooks from './components/SearchBooks';
-import * as BooksAPI from './utils/BooksAPI';
-import './App.css';
+import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
+import * as BooksAPI from '../utils/BooksAPI';
+import '../styles/App.css';
 
 class BooksApp extends React.Component {
 
